ff70654defe728cdd303731433abbd9e64d16fa3
Christoph Ott <christoph.ott@lean-coders.at>
2016-09-18 20:41:16 +0200

    removed verbose output from webpack dev

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -57,7 +57,23 @@ module.exports = function(options) {
                 aggregateTimeout: 300,
                 poll: 1000
             },
-            outputPath: helpers.root('dist')
+            outputPath: helpers.root('dist'),
+            stats: {
+                colors: true,
+                hash: false,
+                version: false,
+                timings: false,
+                assets: false,
+                chunks: false,
+                modules: false,
+                reasons: false,
+                children: false,
+                source: false,
+                errors: false,
+                errorDetails: false,
+                warnings: false,
+                publicPath: false
+            }
         },
         node: {
             global: 'window',
