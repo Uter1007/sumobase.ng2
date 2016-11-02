6db7f9932064b25a9d6a0481a96410878718b5a7
Matthias Posch <posch.matthias@gmail.com>
2016-11-02 11:54:57 +0200

    chore: remove unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,32 +5,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
-import { RouterModule } from '@angular/router';
 
-import {MdButtonToggleModule} from '@angular2-material/button-toggle/button-toggle';
-import {MdButtonModule} from '@angular2-material/button/button';
-import {MdCheckboxModule} from '@angular2-material/checkbox/checkbox';
-import {MdRadioModule} from '@angular2-material/radio/radio';
-import {MdSlideToggleModule} from '@angular2-material/slide-toggle/slide-toggle';
-import {MdSliderModule} from '@angular2-material/slider/slider';
-import {MdSidenavModule} from '@angular2-material/sidenav/sidenav';
-import {MdListModule} from '@angular2-material/list/list';
-import {MdGridListModule} from '@angular2-material/grid-list/grid-list';
-import {MdCardModule} from '@angular2-material/card/card';
-import {MdIconModule} from '@angular2-material/icon/icon';
-import {MdProgressCircleModule} from '@angular2-material/progress-circle/progress-circle';
-import {MdProgressBarModule} from '@angular2-material/progress-bar/progress-bar';
-import {MdInputModule} from '@angular2-material/input/input';
-import {MdTabsModule} from '@angular2-material/tabs/tabs';
-import {MdToolbarModule} from '@angular2-material/toolbar/toolbar';
-import {MdTooltipModule} from '@angular2-material/tooltip/tooltip';
-import {MdRippleModule} from '@angular2-material/core/ripple/ripple';
-import {PortalModule} from '@angular2-material/core/portal/portal-directives';
-import {OverlayModule} from '@angular2-material/core/overlay/overlay-directives';
-import {MdMenuModule} from '@angular2-material/menu/menu';
-import {RtlModule} from '@angular2-material/core/rtl/dir';
-
-import {Store, StoreModule} from '@ngrx/store';
+import {StoreModule} from '@ngrx/store';
 import {counterReducer} from './reducers/counter.reducer';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {StoreLogMonitorModule, useLogMonitor} from '@ngrx/store-log-monitor';
@@ -43,8 +19,7 @@ import {RouterStoreModule, routerReducer} from '@ngrx/router-store';
         BrowserModule,
         HttpModule,
         FormsModule,
-        MdButtonModule.forRoot(),
-        MdCardModule.forRoot(),
+
         StoreModule.provideStore({
             counter: counterReducer,
             router: routerReducer
@@ -58,6 +33,7 @@ import {RouterStoreModule, routerReducer} from '@ngrx/router-store';
         }),
         RouterStoreModule.connectRouter(),
         StoreLogMonitorModule,
+
         appRoutes
     ],
     declarations: [AppComponent],
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,12 +1,9 @@
 import {NgModule} from '@angular/core/src/metadata/ng_module';
 import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
 import homeRoutes from "./home.routes";
 import {HomeComponent} from './home.component';
 import {MdButtonModule} from '@angular2-material/button/button';
 import {MdCardModule} from '@angular2-material/card';
-import {counterReducer} from '../../reducers/counter.reducer';
-import {StoreModule} from '@ngrx/store';
 
 @NgModule({
     imports: [
